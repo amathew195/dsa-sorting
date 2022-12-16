5e451559f6c3a0f7a3780826b4ac6de3b92dec70
Christopher Lim <cli3338198@gmail.com>
2022-12-16 11:42:21 -0800

    finished sorting

--- merge.js
+++ merge.js
@@ -18,13 +18,44 @@ function merge(nums1, nums2) {
   }
 
   if (nums2[p2]) {
-
     sortedArr.push(...nums2.slice(p2, nums2.length));
   }
 
   return sortedArr;
 }
 
-function mergeSort() { }
+function mergeSort(nums) {
+  if (nums.length === 1) return nums;
+  const mid = Math.floor(nums.length / 2);
+  const left = mergeSort(nums.slice(0, mid));
+  const right = mergeSort(nums.slice(mid));
+  return merge(left, right);
+  /**
+   *
+   *        1, 2, 3, 4
+   *
+   *    1,2       3, 4
+   *  1     2   3     4
+   *
+   *  1,2         3,4
+   *
+   *
+   *      1,2,3,4
+   *
+   *
+   *    merge(m([1,2]))      merge(m([3,4]))
+   *
+   *     merge(m([1])) merge(m[2])      merge(m[3]) merge(m([4]))
+   */
+}
+
+function qSort(nums) {
+  if (nums.length === 1) return nums;
+  const pivot = nums[nums.length - 1];
+  const lesser = nums.filter((n) => n < pivot);
+  const equal = nums.filter((n) => n === pivot);
+  const greater = nums.filter((n) => n > pivot);
+  return [...qSort(lesser), ...qSort(equal), ...qSort(greater)];
+}
 
 module.exports = { merge, mergeSort };
